8e65fd59505fae25927201828aee178ff077a24f
Ivantg01 <96993149+Ivantg01@users.noreply.github.com>
2024-03-02 19:27:13 +0100

    Update PostExpanded.tsx

--- components/PostExpanded.tsx
+++ components/PostExpanded.tsx
@@ -18,13 +18,13 @@ const PostExpanded: FunctionComponent<PostProps> = (props) =>{
                 <input className="input" type="text" id="tittle" name="tittle" value={`${dataPost.tittle}`} readonly/>
 
                 <label for="author">Author:</label>
-                <input className="input" type="text" id="author" name="author" value={`${dataPost.tittle}`} readonly/>
+                <input className="input" type="text" id="author" name="author" value={`${dataPost.author}`} readonly/>
 
                 <label for="date">Date:</label>
                 <input className="input" type="text" id="date" name="date" value={`${dataPost.date.toLocaleDateString()}`} readonly/>
 
                 <label for="content">Content:</label>
-                <textarea className="input" id="content" name="content" value={`${dataPost.tittle}`} readonly></textarea>
+                <textarea className="input" id="content" name="content" value={`${dataPost.content}`} readonly></textarea>
 
             </form>
 
